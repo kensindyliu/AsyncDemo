4b780da0d30624a609788a0859a3610daa0b7a0f
Ken <146753820+kensindyliu@users.noreply.github.com>
2024-03-20 14:46:20 -0500

    Add files via upload

---
+++ AsyncTestService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityService
+{
+    public class AsyncTest
+    {
+        public async Task<int> GetID()
+        {
+            await Task.Delay(3000);
+            return 0;
+        }
+        public async Task<string> GetName()
+        {
+            await Task.Delay(3000);
+            return "Ken";
+        }
+        public async Task<string> GetDescription()
+        {
+            await Task.Delay(3000);
+            return "Ken is a student";
+        }
+        public async Task<string> GetLastName()
+        {
+            await Task.Delay(3000);
+            return "Liu";
+        }
+
+
+
+    }
+}
---
+++ Controllers/AsyncTestController.cs
@@ -0,0 +1,34 @@
+﻿using Entities.ViewModels;
+using EntityService;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+
+namespace FinalLab_ORM.Controllers
+{
+    public class AsyncTestController : Controller
+    {
+
+         
+        public async Task<IActionResult> Index()
+        {
+            AsyncTest ats = new();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+            AsyncTestVM ts = new AsyncTestVM();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Task<int> id = ats.GetID(); 
+            Task<string> name = ats.GetName();
+            Task<string> descriptiong = ats.GetDescription();
+            Task<string> lastName = ats.GetLastName();
+            await Task.WhenAll(id, name, descriptiong, lastName);
+            ts.ID = id.Result;
+            ts.Name=name.Result;
+            ts.Description = descriptiong.Result;
+            ts.LastName = lastName.Result;
+            stopwatch.Stop();
+            ViewBag.TimeConsumption = stopwatch.ElapsedMilliseconds;
+            return View(ts);
+        }
+    }
+}
